c4027147637bf9bfcada8e28633e9ea25d3ba546
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2026-02-23 10:18:41 +0100

    Update Practice Areas.tsx

--- app/components/Home/Practice Areas.tsx
+++ app/components/Home/Practice Areas.tsx
@@ -60,9 +60,9 @@ const Practice = () => {
       {/* Show More / Show Less */}
       <button
         onClick={() => setShowAll(!showAll)}
-        className="mt-8 px-6 py-3 bg-[#FFA500] hover:bg-[#F4C430] rounded-full text-white font-semibold transition-colors duration-300"
+        className="mt-8 px-6 py-3  hover:bg-[#F4C430] rounded-full text-[#5F021F] font-semibold transition-colors duration-300"
       >
-        {showAll ? "Show Less" : "Show More"}
+        {showAll ? "Show Less.." : "Show More.."}
       </button>
     </section>
   );
